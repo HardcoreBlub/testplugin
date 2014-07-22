19a5697661043ca60ba7948c59685d4b5a015e0d
HardcoreBlub <maximilian.burr@outlook.de>
2014-07-22 20:21:06 +0200

    FIRST TEST

--- FirstPlugin/src/com/gmail/hardcoreblub123/testplugin/main.java
+++ FirstPlugin/src/com/gmail/hardcoreblub123/testplugin/main.java
@@ -1,11 +1,14 @@
 package com.gmail.hardcoreblub123.testplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util
+import java.util.logging.Logger;
 
 public class main extends JavaPlugin {
 	public void onEnable(){
-		this.getLogger();
-		log.info("Your Plugin has been enabled")
+		this.getLogger().info("Your Plugin has been enabled");		
 	}
+	public void onDisable(){
 }
+}
+
+
