6a2e0262e53642892d15929c981c68816790a1f0
HardcoreBlub <maximilian.burr@outlook.de>
2014-07-22 20:25:26 +0200

    Changing from Logger to System.out.print

--- FirstPlugin/src/com/gmail/hardcoreblub123/testplugin/main.java
+++ FirstPlugin/src/com/gmail/hardcoreblub123/testplugin/main.java
@@ -1,13 +1,13 @@
 package com.gmail.hardcoreblub123.testplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.logging.Logger;
 
 public class main extends JavaPlugin {
 	public void onEnable(){
-		this.getLogger().info("Your Plugin has been enabled");		
+		System.out.println("The Plugin has been enabled");
 	}
 	public void onDisable(){
+		System.out.println("The Plugin has been disabled");
 }
 }
 
