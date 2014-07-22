74bfc07b3741a3c5c5b499176c879c134f650f8c
HardcoreBlub <maximilian.burr@outlook.de>
2014-07-22 20:56:56 +0200

    Implent Command TEST

--- FirstPlugin/src/com/gmail/hardcoreblub123/testplugin/main.java
+++ FirstPlugin/src/com/gmail/hardcoreblub123/testplugin/main.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class main extends JavaPlugin {
 	public void onEnable(){
 		System.out.println("The Plugin has been enabled");
+		getCommand("test").setExecutor(new Commands());
 	}
 	public void onDisable(){
 		System.out.println("The Plugin has been disabled");
