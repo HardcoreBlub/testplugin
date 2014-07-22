b820bbd210bb8221552b832563071d1a9d371175
HardcoreBlub <maximilian.burr@outlook.de>
2014-07-22 20:10:46 +0200

    Test

---
+++ FirstPlugin/src/com/gmail/hardcoreblub123/testplugin/main.java
@@ -0,0 +1,11 @@
+package com.gmail.hardcoreblub123.testplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util
+
+public class main extends JavaPlugin {
+	public void onEnable(){
+		this.getLogger();
+		log.info("Your Plugin has been enabled")
+	}
+}
--- FirstPlugin/src/com/gmail/hardcoreblub123/testplugin/testplugin.java
+++
@@ -1,5 +0,0 @@
-package com.gmail.hardcoreblub123.testplugin;
-
-public class testplugin {
-
-}
